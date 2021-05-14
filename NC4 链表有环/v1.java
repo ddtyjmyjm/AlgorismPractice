4f2b871d647e0103b31813e8304e36e0b4b642ee
ddtyjmyjm <ddtyjmyjm@gmail.com>
2021-05-15 00:08:42 +0800

    add NC4

---
+++ NC4 链表有环/v1.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null)
+            return false;
+        
+        ListNode f = head.next;
+        ListNode s = head;
+        
+        while(f!=null && f.next!=null){
+            if(f == s)
+                return true;
+            s = s.next;
+            f = f.next;
+            if(f == s)
+                return true;
+            f = f.next;
+        }
+        return false;
+    }
+}
