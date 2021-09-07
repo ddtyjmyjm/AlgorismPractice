754e122664146f7d44f9b01d6f688c503f3d1693
oxygenkun <oxygenkun.1@gmail.com>
2021-09-07 17:46:45 +0800

    update lc64

--- leetcode/64最小路径/main.cpp
+++ leetcode/64最小路径/main.cpp
@@ -8,27 +8,21 @@ public:
     size_t const coloums = grid.size();
     size_t const rows = grid[0].size();
     // cache of former length
-    auto cache = vector<vector<int>>(2, vector<int>(rows, 0));
-    // single choice length
-    std::partial_sum(grid[0].begin(), grid[0].end(), cache[0].begin());
+    auto cache = vector<int>(rows, std::numeric_limits<int>::max());
+    cache[0] = 0;
     // next level
-    size_t cache_now = 0, cache_pre = 0;
-    for (size_t i = 1; i < coloums; ++i) {
-      cache_now = i % 2;
-      cache_pre = (cache_now + 1) % 2;
-      
-      cache[cache_now][0] = grid[i][0] +cache[cache_pre][0];
+    for (size_t i = 0; i < coloums; ++i) {
+      cache[0] = grid[i][0] +cache[0];
       for (size_t j = 1; j < rows; ++j) {
-        cache[cache_now][j] =
-            grid[i][j] + std::min(cache[cache_now][j-1], cache[cache_pre][j]);
+        cache[j] = grid[i][j] + std::min(cache[j-1], cache[j]);
       }
     }
-    return cache[cache_now][rows-1];
+    return cache[rows-1];
   }
 };
 int main() {
   Solution s{};
-  auto g = vector<vector<int>>{{1, 2},{1, 1}};
+  auto g = vector<vector<int>>{{1, 3, 1},{1, 5, 1},{4, 2, 1}};
   auto ans = s.minPathSum(g);
-  assert(ans == 3);
+  assert(ans == 7);
 }
